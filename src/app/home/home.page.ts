1c10619d4ce22ee289b745a10ed96d8552eee5e3
Dennis Every <dennis@ansyra.com>
2019-10-13 18:49:36 -0400

    Added crud service

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { StorageService, Item } from '../services/storage.service';
+import { IonList, Platform, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,66 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+ 
+  items: Item[] = [];
+ 
+  newItem: Item = <Item>{};
+ 
+  @ViewChild('mylist', {static: false})mylist: IonList;
+ 
+  constructor(private storageService: StorageService, private plt: Platform, private toastController: ToastController) {
+    this.plt.ready().then(() => {
+      this.loadItems();
+    });
+  }
+ 
+  // CREATE
+  addItem() {
+    this.newItem.modified = Date.now();
+    this.newItem.id = Date.now();
+ 
+    this.storageService.addItem(this.newItem).then(item => {
+      this.newItem = <Item>{};
+      this.showToast('Item added!')
+      this.loadItems(); // Or add it to the array directly
+    });
+  }
+ 
+  // READ
+  loadItems() {
+    this.storageService.getItems().then(items => {
+      this.items = items;
+    });
+  }
 
-  constructor() {}
+  // UPDATE
+  updateItem(item: Item) {
+    item.title = `UPDATED: ${item.title}`;
+    item.modified = Date.now();
 
+    this.storageService.updateItem(item).then(item => {
+      this.showToast('Item updated!');
+      this.mylist.closeSlidingItems(); // Fix or sliding is stuck afterwards
+      this.loadItems(); // Or update it inside the array directly
+    });
+  }
+
+  // DELETE
+  deleteItem(item: Item) {
+    this.storageService.deleteItem(item.id).then(item => {
+      this.showToast('Item removed!');
+      this.mylist.closeSlidingItems(); // Fix or sliding is stuck afterwards
+      this.loadItems(); // Or splice it from the array directly
+    });
+  }
+
+  // Helper
+  async showToast(msg) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 2000
+    });
+    toast.present();
+  }
+ 
 }
